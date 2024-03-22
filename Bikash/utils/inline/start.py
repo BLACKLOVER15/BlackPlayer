b9a6ca9187c81d5eb6c1fbd21ec7807390a998f1
Black Lover <158266786+BLACKLOVER15@users.noreply.github.com>
2024-03-22 13:03:41 +0530

    Update start.py

--- Bikash/utils/inline/start.py
+++ Bikash/utils/inline/start.py
@@ -29,7 +29,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 Youtube 📱", url=f"https://youtube.com/@BikashGadgetsTech"
+                text="📱 Youtube 📱", url=f"https://youtube.com/@techboydeepak?si=5UIQW6PWPH9NL5bX"
             )
         ],
      ]
@@ -59,7 +59,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 YouTube 📱", url=f"https://youtube.com/@BikashGadgetsTech"
+                text="📱 YouTube 📱", url=f"https://youtube.com/@techboydeepak?si=5UIQW6PWPH9NL5bX"
             )
         ],
         [
